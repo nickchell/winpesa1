5cf3aa48be05e0a7670cb346e8143419469e5544
nickchell <chelelgon@gmail.com>
2025-03-14 22:42:02 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -151,7 +151,7 @@ function DepositPage({ onBack }: { onBack: () => void }) {
     setMessage('');
 
     try {
-      const response = await fetch('https://tractor-shopzilla-ver-promotion.trycloudflare.com ', {
+      const response = await fetch('https://tractor-shopzilla-ver-promotion.trycloudflare.com/api/deposit', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
